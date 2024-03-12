5d4992534f892d4d97802e979c02d8a42ea5b152
Alpay <e217125@metu.edu.tr>
2024-03-13 01:22:15 +0300

    some printing

--- server.py
+++ server.py
@@ -5,6 +5,9 @@ from flask import Flask
 app = Flask(__name__)
 
 # Define your routes and other Flask configurations here
+@app.route('/')
+def index():
+    return 'Welcome to my Flask application!'
 
 if __name__ == "__main__":
     print("Server is running")
