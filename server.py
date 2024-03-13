bfd12066c1054444787e210fe5c2bfd36d738ad6
Alpay <e217125@metu.edu.tr>
2024-03-13 12:58:55 +0300

    register.html

--- server.py
+++ server.py
@@ -34,12 +34,22 @@ def register():
         users = mongo.db.users
         existing_user = users.find_one({'name' : request.form['username']})
 
-        if existing_user is None:
-            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'),bcrypt.gensalt())
-            users.insert({'name' : request.form['username'], 'password' : hashpass})
-            session['username'] = request.form['username']
+        if not request.form['email'].endswith('@ceng.metu.edu.tr'):
+            return 'Registration is only allowed for CENG emails.'
+        
+         if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+            # Store additional details
+            users.insert_one({
+                'email': request.form['email'],
+                'password': hashpass,
+                'name': request.form['name'],
+                'phone': request.form['phone']
+            })
+            session['username'] = request.form['email']  # Consider using a more specific session key
             return redirect(url_for('index'))
-        return 'That username already exists!'
+        return 'That email already exists!'
+
     return render_template('register.html')
 
 if __name__ == "__main__":
