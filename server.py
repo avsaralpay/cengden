880b3f7428f2a0eff875bc15525661369c85b763
Alpay <e217125@metu.edu.tr>
2024-03-13 11:30:09 +0300

    added index.html

--- server.py
+++ server.py
@@ -1,14 +1,21 @@
 #cengden server.py dosyası
+#MongoDB url = mongodb+srv://aavsaralpay:<password>@cluster0.khgwljs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+from flask import Flask,render_template,url_for,request,session,redirect
+from flask_pymongo import PyMongo
+import bcrypt
 
-from flask import Flask
 
 app = Flask(__name__)
-
+app.config['MONGO_DBNAME'] = 'aavsaralpay'
+app.config['MONGO_URI'] = 'mongodb+srv://aavsaralpay:<Alpicik123.>@cluster0.khgwljs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 # Define your routes and other Flask configurations here
+
+mongo = PyMongo(app)
+
 @app.route('/')
 def index():
-    return 'Welcome to my Flask application!'
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    print("Server is running")
-    app.run()
+    app.secret_key = "secret_key"
+    app.run(debug=True)
