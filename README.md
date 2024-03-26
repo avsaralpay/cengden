65d5f72e1ace4aa7ee0bcdf9574d06b6da390886
Alpay <e217125@metu.edu.tr>
2024-03-26 17:42:56 +0300

    sssssssssssss

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # cengden
 E-commerce web application based on cloud services
+Author : Alpay Avşar 
+Student id : 2171254
+
+# CENGden Server Functionality
+
+## Functions and Routes
+
+- `send_verification_email(email_to, name, verification_code)`: Sends a verification email to the user with a unique code for account verification.
+
+- `generate_verification_code(length)`: Generates a random alphanumeric string used as a verification code for new user registration.
+
+- `@app.route('/verify', methods=['POST'])`: Processes user verification by comparing input codes with the stored verification codes and if verified registers user to database.
+
+- `@app.route('/')`: The homepage route that parses index.html and displays latest items.
+
+- `@app.route('/items')`: Displays items posted by the currently logged-in user.
+
+- `@app.route('/favorites')`: Shows the favorited items of the logged-in user.
+
+- `@app.route('/delete_item/<item_id>')`: Allows the logged-in user to delete an item they posted.
+
+- `@app.route('/category/<category_name>')`: Displays items filtered by the selected category.
+
+- `@app.route('/login', methods=['GET', 'POST'])`: Handles the user login process.
+
+- `@app.route('/logout')`: Logs out the current user and clears their session.
+
+- `@app.route('/register', methods=['POST', 'GET'])`: Manages the registration process for new users.
+
+- `@app.route('/add_or_update_item', methods=['POST', 'GET'], defaults={'item_id': None})`: Allows users to add new items or update existing ones.
+
+- `notify_price_drop(item_id, old_price, new_price)`: Sends an email notification to users who favorited an item if its price drops.
+
+- `send_price_drop_email(email_to, old_price, new_price)`: Constructs and sends the price drop notification email.
+
+- `@app.route('/item/<item_id>')`: Displays the details of a specific item.
+
+- `@app.route('/deactivate_item/<item_id>')`: Deactivates a listed item by the owner.
+
+- `@app.route('/activate_item/<item_id>')`: Reactivates a deactivated item by the owner.
+
+- `@app.route('/add_to_favorites/<item_id>', methods=['POST'])`: Allows users to add an item to their favorites.
+
+- `@app.route('/remove_from_favorites/<item_id>', methods=['POST'])`: Allows users to remove an item from their favorites.
+
+- `@app.route('/account', methods=['GET', 'POST'])`: Enables users to view and update their account information.
+
+- `@app.route('/admin')`: Admin panel showing all users and items, allowing admin actions such as delete item or user.
+
+- `@app.route('/delete_user/<user_id>')`: Allows the admin to delete a user and their items.
+
+- `@app.route('/delete_item_admin/<item_id>')`: Allows the admin to delete any item.
+
+## Additional Information
+
+The application uses Flask for the backend, PyMongo for interacting with the MongoDB database, bcrypt for password hashing, and SendGrid for sending emails.
+
+I registered e1111111@ceng.metu.edu.tr as admin and one can sign in entering password 11111111
+I registered e2222222@ceng.metu.edu.tr as authenticated user and one can sign in entering password 22222222
+User can update/delete/reactivate and deactivate theirs posts through my items page.
+User can add an item through add item page.
+User can add item to favorites in item detail page, display favourited items through my favorites page.
+User can update account setting in my account page.
+Admins can delete users/items through admin panel page.
+
+
--- server.py
+++ server.py
@@ -15,7 +15,6 @@ app = Flask(__name__)
 app.secret_key = 'secret_key'
 app.config['MONGO_DBNAME'] = 'cengden'
 app.config['MONGO_URI'] = 'mongodb+srv://aavsaralpay:Alpicik123.@cluster0.khgwljs.mongodb.net/cengden?retryWrites=true&w=majority&appName=Cluster0'
-# Define your routes and other Flask configurations here
 
 mongo = PyMongo(app)
 
@@ -62,25 +61,23 @@ def verify():
 
 @app.route('/')
 def index():
-    # This route will show all active items to any user
     items = mongo.db.items.find({'active': True}).sort("timestamp", -1)
     return render_template('index.html', items=list(items), myitems=False)
 
+
 @app.route('/items')
 def my_items():
-    # Find items posted by the logged-in user
     items = mongo.db.items.find({'user_email': session['email']}).sort("timestamp", -1)
     return render_template('index.html', items=list(items), myitems=True)
 
 @app.route('/favorites')
 def favorites():
     if 'email' not in session:
-        return redirect(url_for('login'))  # Ensure user is logged in
+        return redirect(url_for('login')) 
 
     user = mongo.db.users.find_one({'email': session['email']})
     favorite_ids = user.get('favorites', [])
     favorite_object_ids = [ObjectId(id) for id in favorite_ids]
-    # Fetch the favorite items based on their IDs
     favorite_items = mongo.db.items.find({'_id': {'$in': favorite_object_ids}})
 
     return render_template('index.html', items=list(favorite_items), viewing_favorites=True)
@@ -88,17 +85,15 @@ def favorites():
 @app.route('/delete_item/<item_id>')
 def delete_item(item_id):
     if 'email' not in session:
-        return redirect(url_for('login'))  # Ensure user is logged in
+        return redirect(url_for('login'))  
 
-    # Fetch the item to check if the current user is the owner
     item = mongo.db.items.find_one({'_id': ObjectId(item_id)})
 
-    # Check if the item exists and the logged-in user is the owner
     if item and session['email'] == item.get('user_email'):
         mongo.db.users.update_many({}, {'$pull': {'favorites': ObjectId(item_id)}})
         mongo.db.items.delete_one({'_id': ObjectId(item_id)})
 
-    return redirect(url_for('index'))  # Redirect user after deletion
+    return redirect(url_for('index')) 
 
 @app.route('/category/<category_name>')
 def category(category_name):
@@ -110,7 +105,7 @@ def login():
 
     if request.method == 'POST':
         users = mongo.db.users
-        email = request.form.get('email')  # Use .get for safer form data access
+        email = request.form.get('email')  
         login_user = users.find_one({'email': email})
         user_role = login_user.get('role') if login_user else None
         if login_user and bcrypt.checkpw(request.form.get('pass').encode('utf-8'), login_user['password']):
@@ -118,24 +113,22 @@ def login():
             session['role'] = user_role
             return redirect(url_for('index'))
         else:
-            # Use a generic error message for security reasons
             error_message = 'Invalid email or password'
             return render_template('login.html', error=error_message)
     
-    # For a GET request, just show the login form
     return render_template('login.html')
 
 @app.route('/logout')
 def logout():
-    session.pop('email', None)  # Remove user's email from session
+    session.pop('email', None)  
     return redirect(url_for('index'))
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
         users = mongo.db.users
-        temp_verifications = mongo.db.temp_verifications  # Temporary collection for verification codes
-        email = request.form['email']  # Define email here to use it throughout
+        temp_verifications = mongo.db.temp_verifications  
+        email = request.form['email']  
         if not email.endswith('@ceng.metu.edu.tr'):
             error_message = 'Registration is only allowed for CENG emails.'
             return render_template('register.html', error=error_message)
@@ -144,7 +137,6 @@ def register():
 
         if existing_user is None:
             verification_code = generate_verification_code()
-            # Check if a temporary user already exists
             existing_temp_user = temp_verifications.find_one({'email': email})
             if existing_temp_user:
                 temp_verifications.update_one(
@@ -162,7 +154,7 @@ def register():
                     'favorites': []
                 })
             send_verification_email(email, request.form['name'], verification_code)
-            session['email'] = email  # Consider using a more specific session key for email
+            session['email'] = email  
             return render_template('verify.html', email=email)
         
         error_message = 'That email already exists!'
@@ -174,22 +166,20 @@ def register():
 @app.route('/add_or_update_item',methods=['POST','GET'], defaults={'item_id': None})
 @app.route('/add_or_update_item/<item_id>', methods=['POST','GET'])
 def add_or_update_item(item_id=None):
-    # Make sure user is logged in
     if 'email' not in session:
         return redirect(url_for('login'))
     
-    is_update = bool(item_id)  # True if updating, False if adding
-    item_data = {}  # Initialize empty dict for new item or updates
+    is_update = bool(item_id)  
+    item_data = {}  
     old_price = None
     if item_id:
-        # For update, find the existing item
         item = mongo.db.items.find_one({'_id': ObjectId(item_id), 'user_email': session['email']})
         old_price = float(item['price']) if 'price' in item else None
         print(old_price)
         if not item:
-            return redirect(url_for('index'))  # Redirect if item not found or doesn't belong to user
+            return redirect(url_for('index'))  
     else:
-        item = None  # For add, item remains None
+        item = None 
 
     if request.method == 'POST':
         category = request.form.get('category')
@@ -198,7 +188,6 @@ def add_or_update_item(item_id=None):
         description = request.form.get('description', '').strip()
         image_link = request.form.get('image_link', '').strip()
         
-        # Handle additional fields based on category
         item_data = {
             'category': category,
             'user_email': session['email'],
@@ -206,45 +195,40 @@ def add_or_update_item(item_id=None):
             'price': price,
             'description': description,
             'timestamp': datetime.utcnow(),
-            'active': True  # Set active status to True by default
+            'active': True  
         }
         new_price = float(price) if price else None
         print(new_price)
         if old_price and new_price and new_price < old_price:
             print('Price drop detected!')
             notify_price_drop(item_id, old_price, new_price)
-        # Only add image link if it's provided
         if image_link:
             item_data['image_link'] = image_link
         else:
             item_data['image_link'] = 'https://via.placeholder.com/300x200.png?text=No+Image'
-        # Add category-specific fields if they are non-empty
         category_fields = {
             'vehicles': ['type', 'brand', 'model', 'year', 'color', 'engine_displacement', 'fuel_type', 'transmission_type', 'mileage'],
             'phones': ['brand', 'model', 'year', 'operating_system', 'processor', 'ram', 'storage', 'battery_capacity'],
             'computers': ['type', 'brand', 'model', 'year', 'processor', 'ram', 'storage', 'graphics_card', 'operating_system'],
             'lessons': ['tutor_name','location','duration']
-            # Add other categories as needed
         }
 
-        # Add category-specific fields to item_data if they exist and are not empty
         for field in category_fields.get(category, []):
             field_value = request.form.get(field, '').strip()
             if field_value:
                 item_data[field] = field_value
 
-        # Special handling for arrays like camera specifications
         if category == 'phones':
             camera_specs = [{'Type': t.strip(), 'MP': mp.strip()} for t, mp in zip(request.form.getlist('camera_specs_type[]'), request.form.getlist('camera_specs_mp[]')) if t.strip() and mp.strip()]
             if camera_specs:
                 item_data['camera_specifications'] = camera_specs
 
         if category == 'lessons':
-            lesson_topics = request.form.getlist('lessons[]')  # Assuming the form fields for lessons are named 'lessons[]'
-            clean_lesson_topics = [topic.strip() for topic in lesson_topics if topic.strip()]  # Remove empty or whitespace-only topics
+            lesson_topics = request.form.getlist('lessons[]')  
+            clean_lesson_topics = [topic.strip() for topic in lesson_topics if topic.strip()] 
 
             if clean_lesson_topics:
-                item_data['lessons'] = clean_lesson_topics  # Only add the lessons list if it's not empty
+                item_data['lessons'] = clean_lesson_topics  
 
         if item_id:
             mongo.db.items.update_one({'_id': ObjectId(item_id)}, {'$set': item_data})
@@ -280,7 +264,6 @@ def send_price_drop_email(email_to, old_price, new_price):
 
 @app.route('/item/<item_id>')
 def item_detail(item_id):
-    # Convert item_id from the URL to ObjectId
     object_item_id = ObjectId(item_id)
     item = mongo.db.items.find_one({'_id': object_item_id})
     user_info = None
@@ -293,9 +276,8 @@ def item_detail(item_id):
     if 'email' in session:
         session_user = mongo.db.users.find_one({'email': session['email']})
         if session_user:
-            # Ensure comparison is done between similar types
             user_favorites = [str(favorite) for favorite in session_user.get('favorites', [])]
-            is_favorite = item_id in user_favorites  # item_id is a string here
+            is_favorite = item_id in user_favorites  
 
     return render_template('item_detail.html', item=item, user=user_info, is_favorite=is_favorite)
 
@@ -304,7 +286,7 @@ def item_detail(item_id):
 @app.route('/deactivate_item/<item_id>')
 def deactivate_item(item_id):
     if 'email' not in session:
-        return redirect(url_for('login'))  # User must be logged in
+        return redirect(url_for('login'))  
 
     mongo.db.items.update_one(
         {'_id': ObjectId(item_id), 'user_email': session['email']},
@@ -315,7 +297,7 @@ def deactivate_item(item_id):
 @app.route('/activate_item/<item_id>')
 def activate_item(item_id):
     if 'email' not in session:
-        return redirect(url_for('login'))  # User must be logged in
+        return redirect(url_for('login'))  
 
     mongo.db.items.update_one(
         {'_id': ObjectId(item_id), 'user_email': session['email']},
@@ -357,15 +339,12 @@ def account():
         new_phone = request.form.get('phone')
         update_data = {}
 
-        # Update email if provided and different from the current one
         if new_email and new_email != session['email']:
             update_data['email'] = new_email
-            session['email'] = new_email  # Update session email
-            # Update the contact information in the user's posted items
+            session['email'] = new_email  
             items = mongo.db.items
             items.update_many({'user_email': user['email']}, {'$set': {'user_email': new_email}})
 
-        # Update phone if provided
         if new_phone and new_phone != user.get('phone'):
             update_data['phone'] = new_phone
 
@@ -382,7 +361,7 @@ def admin_panel():
     if 'email' not in session or 'role' not in session:
         return redirect(url_for('login'))
     if session['role'] != 'admin':
-        return "You do not have access to this page", 403  # Or redirect to another page
+        return "You do not have access to this page", 403 
     
     users = mongo.db.users.find({})
     items = mongo.db.items.find({})
@@ -392,8 +371,8 @@ def admin_panel():
 def delete_user(user_id):
     if 'email' not in session or 'role' not in session or session['role'] != 'admin':
         return "Unauthorized", 403
-    if user:  # If user exists
-        # Find all items posted by the user
+    user = mongo.db.users.find_one({'_id': ObjectId(user_id)})
+    if user:  
         items = mongo.db.items.find({'user_email': user['email']})
         item_ids = [item['_id'] for item in items]
 
